0dbc68a45c50ecb82708ec1e6166227a07d173a9
Arthur Silva Dantas <arthur.dantas@estudante.ifms.edu.br>
2023-09-22 20:30:37 -0300

    Criptografando senha no banco

--- servidor/index.js
+++ servidor/index.js
@@ -1,90 +1,113 @@
-const crypto = require('./crypto');
+const crypto = require("./crypto");
 
 // JWT
 require("dotenv-safe").config();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 var { expressjwt: expressJWT } = require("express-jwt");
-const cors = require('cors');
+const cors = require("cors");
 
-var cookieParser = require('cookie-parser')
+var cookieParser = require("cookie-parser");
 
-const express = require('express');
-const { usuario } = require('./models');
+const express = require("express");
+const { usuario } = require("./models");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use(cookieParser());
 app.use(
   expressJWT({
     secret: process.env.SECRET,
     algorithms: ["HS256"],
-    getToken: req => req.cookies.token
-  }).unless({ path: ["/autenticar", "/logar", "/deslogar", "/usuarios/cadastrar"] })
+    getToken: (req) => req.cookies.token,
+  }).unless({
+    path: ["/autenticar", "/logar", "/deslogar", "/usuarios/cadastrar"],
+  })
 );
 
-app.get('/autenticar', async function(req, res){
-  res.render('autenticar');
-})
+app.get("/autenticar", async function (req, res) {
+  res.render("autenticar");
+});
 
-app.get('/', async function(req, res){
-  res.render("home")
-})
+app.get("/", async function (req, res) {
+  res.render("home");
+});
 
-app.get('/usuarios/cadastrar', async function(req, res){
-  res.render('cadastrar')
- })
+app.get("/usuarios/cadastrar", async function (req, res) {
+  res.render("cadastrar");
+});
 
- app.post('/usuarios/cadastrar', async function(req, res){
-  if(req.body.senha == req.body.confirmpass){
-    await usuario.create(req.body);
-    res.redirect("/usuarios/listar")
-    } else{
-      res.json("Não possivel cadastrar")
-    }
- })
-
- app.get('/usuarios/listar', async function(req, res){
+app.get("/usuarios/listar", async function (req, res) {
   try {
     const list = await usuario.findAll();
-    res.render('listar', { list });
+    res.render("listar", { list });
   } catch (error) {
     console.error(error);
     res.status(500).send("Erro ao listar usuários");
   }
 });
 
- app.post('/logar', (req, res) => {
-  if(req.body.user == "Jaco" && req.body.pass == "123"){
-    const id = 1
-      
-    const token = jwt.sign({ id }, process.env.SECRET, {
-      expiresIn: 300
-    })
 
-    res.cookie('token', token, {httpOnly: true});
-    return res.json({
-      usuario: req.body.user, 
-      token: token 
-    })
+
+
+
+
+
+
+
+
+
+app.post("/usuarios/cadastrar", async function (req, res) {
+  if (req.body.senha == req.body.confirmpass) {
+    //A senha esta criptografada no banco de dados
+    let senhaCrypto = crypto.encrypt(req.body.senha);
+    const newUser = await usuario.create({
+      usuario: req.body.usuario,
+      senha: senhaCrypto,
+    });
+    const id = newUser.id;
+    const token = jwt.sign({ id }, process.env.SECRET, { expiresIn: 300 });
+    res.cookie('token', token, { httpOnly: true })
+    res.redirect('/autenticar');
+  } else {
+    res.json("Não possivel cadastrar");
   }
-  res.status(500).json({mensagem: "Login Inválido"})
-})
+});
 
-app.post('/deslogar', function(req, res) {
-  res.cookie('token', null, {httpOnly: true});
-  return res.json({
-    deslogado: true
-  })
-})
+app.post("/logar", (req, res) => {
+  if (req.body.user == "Jaco" && req.body.pass == "123") {
+    const id = 1;
 
-app.listen(3000, function() {
-  console.log('App de Exemplo escutando na porta 3000!')
+    const token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 300,
+    });
+
+    res.cookie("token", token, { httpOnly: true });
+    return res.json({
+      usuario: req.body.user,
+      token: token,
+    });
+  }
+  res.status(500).json({ mensagem: "Login Inválido" });
+});
+
+
+
+
+
+
+app.post("/deslogar", function (req, res) {
+  res.cookie("token", null, { httpOnly: true });
+  res.redirect('/autenticar')
+});
+
+app.listen(3000, function () {
+  console.log("App de Exemplo escutando na porta 3000!");
 });
